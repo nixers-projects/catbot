31904e83a86a3751a81187459b08c88c86146f17
dami0 <sracz0@gmail.com>
2014-06-15 20:28:33 +0100

    my code works. it's 6-100 times more performant. i still don't know why

--- some.go
+++ some.go
@@ -4,7 +4,7 @@
 //	authored: dami0 and dcat (at iotek dot org)
 */
 
-package main
+package some
 
 // check for the presence of string in string array, return -1 or index
 func Present (p string, a []string) int {
@@ -14,27 +14,37 @@ func Present (p string, a []string) int {
 	return -1
 }
 
-// remove string from string array
+// remove (all instances) string from string array
 func Remove (pat string, lst []string) []string {
 	ind := []int {}
 	a := 0; b := 0; dab := 0
-	d := Present(pat,lst)
+	d := Present(pat, lst)
 
-	for i := d; d != -1; i += 1 + d{
+	for i := d; d != -1; i += 1 + d {
 		ind = append(ind, i)
 		d = Present(pat,lst[i+1:])
 	}
-	if len(lst) == len(ind) { return []string {} }
+	ret_l := len(lst) - len(ind)
 
-	out := make([]string, len(lst) - len(ind))
+	if ret_l == len(lst) { return lst }
 	c := ind[0]
-	if ind[0] != 0 { copy(out[:c], lst[:c]) }
+	if ret_l == 0 {
+		return []string {}
+	} else if ret_l == 1 {
+		return []string {}
+	} else if len(ind) == 1 {
+		copy(lst[:c], lst[:c])
+		copy(lst[c:], lst[c+1:])
+		return lst[:ret_l]
+	}
+
+	out := lst
 	for d = 0; d < len(ind) - 1; d++ {
 		a = ind[d] + 1
 		b = ind[d + 1]
 		dab = b - a
-		c += copy(out[c:c+dab], lst[a:b])
+		if dab > 0 { c += copy(out[c:c+dab], lst[a:b]) }
 	}
-	return out
-//	return ind
+	copy(out[c:], lst[ind[d] + 1:])
+	return out[:ret_l]
 }
